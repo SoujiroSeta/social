a5c9e083fd0f524c97a9b5e640ec2bdff984797f
root <root@bt.foo.org>
2013-07-31 14:27:16 +0700

    social

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Social::Application.config.secret_token = '670ed4e0269ef37e474b574ba72069915a96f6d74f208a466974415961cba3881686999f90ee55cd79d47d6d92cb54c78eb8a9d9df3e37435ce8767e9af30d11'
