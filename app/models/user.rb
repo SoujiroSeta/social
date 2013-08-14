d6b758093f8199826887497e39d4f75203230c1e
root <root@bt.foo.org>
2013-08-14 17:06:54 +0700

    layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :confirmable, :lockable,
          :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
-  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :first_name, :last_name, :remember_me
   has_many :questions 
   has_many :comments
   has_and_belongs_to_many :groups
