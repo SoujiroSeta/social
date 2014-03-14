68a6e0d32d3a0c4f0b382ba72d486c636127af2e
agus <soujiroseta@screamvoidz.(none)>
2014-03-14 21:48:37 +0700

    part2

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
-	 $('#cari').on('submit', function(event) {
+	$('#cari').on('submit', function(event) {
 
-    var q = document.forms["cari"]["q"].value;
-    if (q==null || q=="")
-    {
+  	var q = document.forms["cari"]["q"].value;
+    if (q==null || q=="") {
 	  alert("gk boleh kosong");
 	  	return false;
-	  }
-	  else{
+	  } else {
 	  	return true;
 	  }
 	  
- });
+  });
 })
 
--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Social
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.assets.initialize_on_precompile = false
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
@@ -53,7 +53,7 @@ module Social
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-
+    
     # Enable the asset pipeline
     config.assets.enabled = true
 
